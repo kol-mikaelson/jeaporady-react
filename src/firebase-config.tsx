b51bcd1f65e96419cdec3ddd1da63cea247bde05
kol-mikaelson <87298613+kol-mikaelson@users.noreply.github.com>
2025-02-23 22:08:44 +0530

    Update firebase-config.tsx

--- src/firebase-config.tsx
+++ src/firebase-config.tsx
@@ -6,13 +6,6 @@ import { getFirestore } from 'firebase/firestore';// TODO: Add SDKs for Firebase
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC6J8W8OQkdbLw_UVZcAqeNBGz4st0bc9E",
-  authDomain: "litsoc-jeaporady.firebaseapp.com",
-  projectId: "litsoc-jeaporady",
-  storageBucket: "litsoc-jeaporady.firebasestorage.app",
-  messagingSenderId: "1000912160668",
-  appId: "1:1000912160668:web:01d38f6c3ffc58af896a59",
-  measurementId: "G-35P205C06S"
 };
 
 // Initialize Firebase
